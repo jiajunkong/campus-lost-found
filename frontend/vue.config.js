f20af3164de16938c0651b673a6b4887976d5c1a
jiajunkong <2559399532@qq.com>
2026-06-30 21:29:23 +0800

    fix: use root publicPath for Render static site

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: './',
+  publicPath: '/',
   devServer: {
     port: 8081,
     open: true,
